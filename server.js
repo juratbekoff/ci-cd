0908623356d1d9e8c79c088b5b8aa175ab8146fb
unknown <juratbekxudayberganov9@gmail.com>
2022-10-27 21:19:12 +0500

    next atempt for ookokok

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('ay amalladingay juratbek')
+  res.send('ikkinchi urinishda')
 })
 
 app.get('/ok', (req, res) => {
