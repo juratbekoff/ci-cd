132edbbb0fb044be419e325487ad7484f7de1c9c
unknown <juratbekxudayberganov9@gmail.com>
2022-10-27 21:10:29 +0500

    last changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! jaloliddinoff')
+  res.send('ay amalladingay juratbek')
 })
 
 app.get('/ok', (req, res) => {
