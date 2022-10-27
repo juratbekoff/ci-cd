82c1804d76b32e580f6a62e3dbbba4b1a7de10e2
unknown <juratbekxudayberganov9@gmail.com>
2022-10-27 19:28:05 +0500

    next

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! juratbek')
+  res.send('Hello World! juratbek na gap jaloliddin')
 })
 
 app.listen(port, () => {
